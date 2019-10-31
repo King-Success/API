beb9496a789f91a67abaf9da1742a66007bc469f
G-Chilie <okonkwolinda7@gmail.com>
2019-10-31 12:50:56 +0100

    app setup

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import routes from "./src/routes";
-​
-const app = express();
+​const app = express();
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
---
+++ src/controller/index.js
@@ -0,0 +1,13 @@
+/**
+ * Houses the controller methods
+ */
+export default class IndexController {
+    /**
+     *
+     * @param {object} req
+     * @param {object} res
+     */
+    static create(req, res) {
+    }
+  }
+  
