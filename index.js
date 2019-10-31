98fc05f0136427e218fa3f46961fdcf1a99cdf17
G-Chilie <code4kings@gmail.com>
2019-10-31 13:18:18 +0100

    fix server error

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 import express from "express";
 import morgan from "morgan";
 import routes from "./src/routes";
-​const app = express();
+const app = express();
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-​
+
 // connect app to routese
 app.use(routes);
-​
+
 // / catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
-​
+
 // finally, let's start our server...
 const server = app.listen(process.env.PORT || 3000, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening on port ${server.address().port}`);
 });
-​
+
 export default app;
--- src/controller/index.js
+++ src/controller/index.js
@@ -2,12 +2,10 @@
  * Houses the controller methods
  */
 export default class IndexController {
-    /**
-     *
-     * @param {object} req
-     * @param {object} res
-     */
-    static create(req, res) {
-    }
-  }
-  
+  /**
+   *
+   * @param {object} req
+   * @param {object} res
+   */
+  static create(req, res) {}
+}
